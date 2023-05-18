a08326dc8262aeb5d3ddef7acac315a205f2311e
ryeongse25 <seryeong971120@gmail.com>
2023-05-18 16:22:31 +0900

    feat: connect MongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
-import { Request, Response } from "express";
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import yagujangRoute from "./routes/yagujang";
 
 const app = express();
 const port = 8000;
+dotenv.config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
+
+app.use("/yagujang", yagujangRoute);
+
+if (process.env.MONGODB_URL)
+  mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => {
+      console.log("MongoDB connected");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 
 app.listen(port, () => {
   console.log("Server Port : ", port);
